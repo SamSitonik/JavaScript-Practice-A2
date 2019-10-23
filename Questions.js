ecc1aadaf908d97dd0f0eb5d63aa4637dbac105a
Sam Sitonik <SamSitonik@outlook.com>
2019-10-23 15:48:35 -0400

    answersToSix

--- Questions.js
+++ Questions.js
@@ -10,29 +10,48 @@ function printInt(n) {
 
 printInt(10);
 // TEST everything on index.HTML. 
-// don't ...FIND on VIDEO
 
 // Q2: Write the function printIntRev(n) taking one parameter n and print all natural numbers in reverse in console (from n to 1).
 // Hint: reverse loop in Q1 (same as above)
+// WRONG! 
 function printIntRev(n) {
  
 	for(let i = 1; i>=1 ; i--) {
+            // console.log it out :)
 		console.log(i);
     }
 }
 printIntRev(50)
 
-// Q3: Write the function checkInput(x) taking one parameter x and return the string ‘number’ if x is a number; return the string ‘string’ if x is a string; and return ‘boolean’ if x is a boolean. Otherwise returns -1. 
+// Q3: Write the function checkInput(x) taking one parameter x and return the string ‘number’ if x is a number; return the string ‘string’ if x is a string; and return ‘boolean’ if x is a boolean. Otherwise returns -1. Obvious if...else statement, but 3 other solutions,
 Examples:
 checkInput(5) ====> ‘number’
 checkInput(‘hello’)======> ‘string’
 checkInput(‘5’) ======> ‘string’
 checkInput(true)=====> boolean
 checkInput([1,2,3,4]) =====> -1
-Hint: Check JavaScript typeof
-.....
+Hint: Check JavaScript typeof--"returns the data type of its operand in the form of a string" || "typeof operator returns a string indicating the type of the unevaluated operand."
 
-// Q4:Write the function simpleEvenAdding(num) taking a number and add up all the even numbers from 1 to num, and return it 
+function checkInput(x) {
+    if (typeof x === 0<=n) {return "number";} 
+    else if (typeof x = "") {return "string";}
+    else { return "boolean";}
+}
+// Shiyu broke it down in English 1st
+if(check if x is number) {
+    return number
+    } else if(x is string) {
+    return string 
+    } else if (x is boolean) {
+       return boolean 
+    } else {                // <--"otherwise"
+        return -1
+    }
+    // way to check if 'x' is this/that/other, and that's where 'typeof' comes in. 
+}
+// Soln 2 is a Switch example, Soln 4 is a ternary operator
+
+// Q4:HARD---Shiyu wants us to know how to 1)Analyze 2)Understand the process. Write the function simpleEvenAdding(num) taking a number and add up all the even numbers from 1 to num, and return it 
 Examples: simpleEvenAdding(5) ======> 6 (because 2+4 = 6)
        simpleEvenAdding(10) ======> 30 (because 2+4+6+8+10 = 30)
        simpleEvenAdding(11) ======> 30 (because 2+4+6+8+10 = 30)
@@ -40,14 +59,59 @@ Examples: simpleEvenAdding(5) ======> 6 (because 2+4 = 6)
        simpleEvenAdding(0) =======> 0
 Hint: loop + if conditional + %
 ....
+// loop within a loop(nested)??? or function within a loop?
 
-// Q5:Write the function letterCapitalize(str) taking a string and capitalize the first letter of each word. The given words will be separated by only one space.
+function simpleEvenAdding(num) {
+    // (num=1;1<=n;n++), <--then put (n%2===0) in there somewhere? xy
+    for (var num = 1 ; 1 <= num; num++) { 
+        if(num % 2 === 0) {
+          console.log(simpleEvenAdding(num)); 
+        }
+      }
+    simpleEvenAdding(6)
+/* Not working :( Shiyu-Udacity Loops on Factorials
+ var solution = 1 
+ for(var i=12; i>=0, i--) {
+     solution *= 1
+ }
+ console.log(solution);
+*/ 
+// We need to store the answer somewhere = "solution" 
+// As above, we will use addition instead of multiplication. 
+
+function simpleEvenAdding(num)      // ADD all numbers from 1 to num
+var solution = 
+
+Easy solution, make the loop like this:
+for(var i=0;i<=num;i+=2)
+solution += i; 
+
+// Q5:EVEN HARDER-Write the function letterCapitalize(str) taking a string and capitalize the first letter of each word. The given words will be separated by only one space.
 	Examples:
     letterCapitalize(“hello world”) ===========> “Hello World”
 letterCapitalize(“you cannot find the answer online”) =====>  “You Cannot Find The Answer Online”
 
 Hint: There is a built-in function in JavaScript that change string into uppercase. There is also a function to separate string into an array of words. (split)
+Shiyu: said try to break it down into smaller tasks
+1)separate words= str.split(separator, limit)       // First character?
+    // you can use "slice" OR "substring"
+2)capatilize= str.toUpperCase(0)}
 
+console.log(letterCapitalize("you cannot find this answer online"));
+
+
+function letterCapitalize(str) {
+    var solution = [ ];
+    var words = str.split(" ");
+  
+    for (var i = 0; i < words.length; i++) {
+      solution.push(words[i][0].toUpperCase() + words[i].slice(1);)
+    }
+    return solution.join(" ");
+  
+  }
+  
+  console.log(letterCapitalize("you cannot find the answer online"));
 // Q6: Write the function simpleReverse(str) taking a string and return the string in reversed order. 
 	Examples:
     // simpleReverse(‘hello’) ======> ‘olleh’
@@ -55,7 +119,19 @@ Hint: There is a built-in function in JavaScript that change string into upperca
 
 Hint: Think of how you can loop through a string or array of characters backwards to produce a new string
 
-// Q7: Write the function findDiff(arr) taking an array of numbers as parameter and return the difference between the maximum number and the minimum number (max - min). 
+function simpleReverse(str) {
+      var solution = "";
+      var length = words.length;
+    
+      for (var i = str.length - 1; i>= 0; i--) {
+        solution += str[i];
+        
+      }
+      return solution;
+    }
+    console.log(simpleReverse("I Love Code"));
+
+// Q7: NO idea!!! Write the function findDiff(arr) taking an array of numbers as parameter and return the difference between the maximum number and the minimum number (max - min). 
 Examples:
 findDiff([1,2,4,6,20, 3]) =======> 19 (Because 20 - 1 = 19)
 findDiff([24, 22, 23, 22, 24]) =======> 2 (Because 24 - 22 = 2)
@@ -63,6 +139,8 @@ findDiff([1, 1, 1, 1, 1]) =======> 0
 findDiff([1]) ======> 0;
 findDiff([]) =======> 0;
 Hint: 1)Looking for max, 2)Looking for min 3) min -> max - min.Use a loop to subtract min from max, Check online
+
+function findDiff(arr) {}
 
 // Q8: Write the function timeConvert(num) taking a number as parameter and return the number of hours and minutes the parameter converts to. Separate the number of hours and minutes with a colon.
 	Example:
@@ -72,6 +150,8 @@ Hint: 1)Looking for max, 2)Looking for min 3) min -> max - min.Use a loop to sub
     timeConvert(59)=======> 0:59
     Hint: Dividing(/) and modulo(%) the number 60. NO loop needed!
 
+function timeConvert(num)
+
 // Q9: Write the function findStr(str, long) taking two strings as parameters and return how many str you can find in long. Assume Str is not empty string.
 Example:
 findStr(“hi”, “dasdhidasdahidashi”) =======> 3
@@ -79,6 +159,8 @@ findStr(“o”, “daodo”) =======> 2
 findStr(“ha”,”abcde”)=======> 0
 findStr(“h”, “hihelloho”)======> 3
 Hint: HARD! loop through the long string, to check if there is any "str"
+
+function findStr(str,long)      // <--paramaters
 
 // Q10- Write the function selfDividingNumbers(left, right) taking two number bound as parameters and returns an array of every possible self dividing number between 
 them, including the bounds. 
@@ -89,6 +171,8 @@ Examples:
 	selfDividingNumbers(1, 22) ========> [1,2,3,4,5,6,7,8,9,11,12,15,22]
 	selfDividingNumbers(1, 10) =======> [1,2,3,4,5,6,7,8,9]
 selfDividingNumbers(12, 21) =======> [12, 15]
+
+function selfDividingNumbers(left, right) {}
 
 // Q11-  video
 11. Write the function moveZeros(nums) taking an array of numbers and move all 0’s to the end of it while maintaining the relative order of the non-zero elements.  For example:
@@ -107,6 +191,7 @@ function moveZeros(nums{
     }
 })
 
+
 // Q12- Create an average(nums) function that calculates the average of an array of numbers.
 Examples: 
 	average([2, 6]) =====> 4
@@ -116,3 +201,4 @@ average([]) ======> 0
 
 Hint: Sum of all elements / number of elements
 
+function average(nums)
--- main.js
+++ main.js
@@ -1,43 +1,102 @@
-console.log(doSomething);
-doSomething();
-
-function doSomething() {
-	console.log("Declare Something");
-}
-
 // Final Answers
 // Q1:
-function printInt(n) {
-  for (var i = 1; i <= n; i++) {
-    console.log(i);
-  }
-}
-printInt(0);
 
-// Q2:wrong
-// function printIntReverse(n) {
+// function printInt(n) {
 //   for (var i = 1; i <= n; i++) {
-//     // console.log it out :)
 //     console.log(i);
 //   }
 // }
-// printIntReverse(5);
+
+// printInt(50);
+
+// Q2:
+
+// function printIntRev(n) {
+//   for (var i = n; i >= 1; i--) {
+//     console.log(i);
+//   }
+// }
+
+// printIntRev(50);
+
 // Q3:
+
+// function checkInput(x) {
+//     if (typeof x === "number") {
+//       return "number";
+//     } else if (typeof x === "string") {
+//       return "string";
+//     } else if (typeof x === "boolean") {
+//       return "boolean";
+//     } else {
+//       return -1;
+//     }
+//   }
+// console.log(checkInput("false"));         // string
 
 // Q4:
 
+// function simpleEvenAdding(num) {
+//   var solution = 0;
+
+// for (var i = 1; i <= num; i++) { 
+//     if (i % 2 === 0) { 
+//       solution += i;
+//     }
+//   }
+//   return solution;
+// }
+
+// console.log(simpleEvenAdding(11));
+
 // Q5:
+
+// function letterCapitalize(str) {
+//   var solution = "";
+//   var words = str.split(" ");
+//   var length = words.length;
+
+//   for (var i = 0; i < length; i++) {
+//     solution += " " + words[i][0].toUpperCase() +
+//     words[i].slice(1);
+//   }
+//   return solution.slice(1);
+// }
+// console.log(letterCapitalize("you cannot find the answer online"));
 
 // Q6:
 
+// function simpleReverse(str) {
+//   var solution = "";
+
+//   for (var i = str.length - 1; i>= 0; i--) {
+//     solution += str[i];
+    
+//   }
+//   return solution;
+// }
+// console.log(simpleReverse("I Love Code Hellooooooo"));
+
 // Q7:
+
+function findDiff(arr) {}
 
 // Q8:
 
+// dffsdf
+
 // Q9:
+
+// sl;dflg
 
 // Q10:
 
+// dfkgkldfm
+
 // Q11:
 
+// lsdfkgkmg
+
 // Q12:
+
+// fmgmsfkg
